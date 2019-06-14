bf09309cb8cd4fa731b477628869085a0b5ab460
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-14 19:42:50 -0300

    spring JPA

---
+++ 01_Spring_JPA/src/br/com/fiap/model/Produto.java
@@ -0,0 +1,41 @@
+package br.com.fiap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+
+	@Id
+	private Integer id;
+	private String nome;
+
+	public Produto() {
+	}
+
+	public Produto(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto (id: " + id + ", nome: " + nome + ")";
+	}
+}
